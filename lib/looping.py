6861d0c71950e745e56cbc20f119ae17a477ae58
Maika Harrell <mikahado@gmail.com>
2023-06-21 14:45:18 -0400

    completes

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    count = 10
+    while count > 0:
+        print(count) 
+        count -= 1
+    print("Happy New Year!")
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+     
+    sqrts = list()
+    for numb in int_list: 
+        sqrt = numb * numb
+        sqrts.append(sqrt)
+    return sqrts
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    count = 0
+    while count < 100: 
+        count += 1
+        if count % 3 == 0 and count % 5 == 0:
+            print("FizzBuzz")
+        elif count % 5 == 0:
+            print("Buzz")
+        elif count % 3 == 0: 
+            print("Fizz")
+        else: 
+            print(count)
